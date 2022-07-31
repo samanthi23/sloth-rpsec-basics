4ab6934b6b8b3dc95e6723944642332e94f96409
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2022-07-31 15:47:16 +0000

    coded drink, eat and initialize methods

--- lib/sloth.rb
+++ lib/sloth.rb
@@ -1,13 +1,18 @@
 class Sloth
-    attr_reader :name, :foods
+    attr_reader :name, :foods, :drinks
     
     
     def initialize(name)
        @name = name 
        @foods = []
+       @drinks = {}
     end
     
     def eat(food)
+        @foods << food
+    end
+    
+    def drink(beverage, amount)
         
     end
 end
--- spec/sloth_spec.rb
+++ spec/sloth_spec.rb
@@ -13,8 +13,26 @@ describe Sloth do
         end
     end
     
-    descrobe "#eat" do
+    describe "#eat" do
+       it "adds a new food to the foods array" do
+            expect(sloth.foods).to_not include("leaves")
+            sloth.eat("leaves")
+            expect(sloth.foods).to include("leaves")
+            
+            
+        end
+    end
+    
+    describe "#drink" do 
         
+       it "adds the drink as a key to the drinks hash" do
+           expect(sloth.drinks).to have_key("lemonade")
+       end
+       
+         it "adds the amount as a value to the drinks hash" do
+           
+           expect(sloth.drinks).to have_value("10")
+       end
     end
 
 end
