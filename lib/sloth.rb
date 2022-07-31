cf36e24a6e64b9121c7222a741cdd3f517d2e51d
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2022-07-31 17:22:06 +0000

    modified file

--- lib/sloth.rb
+++ lib/sloth.rb
@@ -1,6 +1,8 @@
 class Sloth
     attr_reader :name, :foods, :drinks
     
+    DIRECTIONS = %w(north south east west)
+    
     
     def initialize(name)
        @name = name 
@@ -13,6 +15,12 @@ class Sloth
     end
     
     def drink(beverage, amount)
-        
+        @drinks[beverage] = amount
+    end
+    
+    def run(direction)
+        raise ArgumentError unless DIRECTIONS.include?(direction)
+       "I'm running #{direction} at .00001 miles per hour!"
+       
     end
 end
--- spec/sloth_spec.rb
+++ spec/sloth_spec.rb
@@ -24,15 +24,25 @@ describe Sloth do
     end
     
     describe "#drink" do 
-        
+       before(:each) { sloth.drink("lemonade", 10) } 
        it "adds the drink as a key to the drinks hash" do
            expect(sloth.drinks).to have_key("lemonade")
        end
        
          it "adds the amount as a value to the drinks hash" do
            
-           expect(sloth.drinks).to have_value("10")
+           expect(sloth.drinks).to have_value(10)
        end
+    end
+    
+    describe "#run" do
+        it "returns a string that includes the direction" do
+            expect(sloth.run("west")).to include("west")
+        end
+        
+        it "raises an ArgumentError if the direction is invalid" do 
+            expect { sloth.run("all over the place") }.to raise_error(ArgumentError )
+        end
     end
 
 end
